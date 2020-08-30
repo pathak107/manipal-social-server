1e688575823adcde94b1120ab815d4e09d98ab38
Shubham <smartshubhampathak@gmail.com>
2020-08-30 14:06:34 +0530

    places controller working and added create place in admin controller

--- controllers/placeController.js
+++ controllers/placeController.js
@@ -1,7 +1,42 @@
 //importing models
 const Place=require('../models/places')
-
-
 exports.place_get_all=(req,res)=>{
     //send list of all the places
+    console.log("fetching all places");
+    Place.find((err,places)=>{
+        console.log(places);
+        if(err){
+            return res.json({
+                status: "failure",
+                message: "Some error occurred in retrieving places from database",
+                error: err,
+            })
+        }
+        //segregate depending upon types
+        var beaches=[];
+        var clubs=[];
+        var resturants=[];
+        places.filter((value)=>{
+            if(value.type=='beach')
+            {
+                beaches.push(value);
+            }
+            else if(value.type=='club'){
+                clubs.push(value);
+            }
+            else if(value.type=='resturant'){
+                resturants.push(value);
+            }
+        })
+        return res.status(200).json({
+            status: "success",
+            message: "Retrived all the places",
+            data: {
+                clubs:clubs,
+                resturants:resturants,
+                beaches:beaches
+            }
+        })
+    })
+    
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var router = express.Router()
 
+//importing admin controller
+const adminController=require('../controllers/adminController');
+
 //register new users
-router.post('/register',(req,res)=>{
-    
-})
+router.post('/login',(req,res)=>{});
+
+router.post('/createPlace',adminController.create_place)
 
 module.exports = router
